9b66add7678e37216d7a5c5aa72ab829619e4e76
Zilloww <raphaelbrancovieira.pro@gmail.com>
2024-02-20 16:10:20 +0100

    feat(notifications-page): init

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,7 @@ class AsynconfApp extends StatefulWidget {
 
 class _AsynconfAppState extends State<AsynconfApp> {
 
-<<<<<<< HEAD
-  int _selectedPageIndex = 0;
-=======
   int _currentPageIndex = 0;
->>>>>>> bottom-navigation-bar
 
   @override
   Widget build(BuildContext context) {
@@ -43,130 +39,53 @@ class _AsynconfAppState extends State<AsynconfApp> {
               color: Colors.white,
             ),
           ),
+          const Text(
+            "Test des notifications",
+            style: TextStyle(
+              fontFamily: "Poppins",
+              color: Colors.white,
+            ),
+          ),
           ][_currentPageIndex],
           backgroundColor: Colors.deepPurple[300],
         ),
         body: [
           const HomePage(),
           const EventsPage(),
-<<<<<<< HEAD
-        ][_selectedPageIndex],
+          const NotificationsPage(),
+        ][_currentPageIndex],
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _selectedPageIndex,
+          currentIndex: _currentPageIndex,
           onTap: (int index) {
             setState(() {
-              _selectedPageIndex = index;
+              _currentPageIndex = index;
             });
           },
           items: const [
-=======
-        ][_currentPageIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          onTap: (int tappedIndex) {
-            setState(() {
-              _currentPageIndex = tappedIndex;
-            });
-          },
-          items: const <BottomNavigationBarItem>[
->>>>>>> bottom-navigation-bar
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: "Accueil",
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.event),
-<<<<<<< HEAD
               label: "Agenda",
             ),
-          ],
-=======
-              label: "Événements",
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications),
+              label: "Notifications",
             ),
-          ]
->>>>>>> bottom-navigation-bar
+          ],
         ),
       )
     );
   }
 }
-<<<<<<< HEAD
-=======
 
-class EventsPage extends StatefulWidget {
-  const EventsPage({super.key});
-
-  @override
-  State<EventsPage> createState() => _EventsPageState();
-}
-
-class _EventsPageState extends State<EventsPage> {
-
-  final _events = [
-    {
-      'title': "Conférence sur le développement durable",
-      'date': "20.04.2024",
-      'location': "Salle de conférence de l'EPFL",
-    },
-    {
-      'title': "Git Basics",
-      'date': "22.04.2024",
-      'location': "Salle de conférence de l'EPFL",
-    },
-    {
-      'title': "Introduction à Flutter",
-      'date': "23.04.2024",
-      'location': "Salle de conférence de l'EPFL",
-    },
-    {
-      'title': "Introduction à Dart",
-      'date': "23.04.2024",
-      'location': "Salle de conférence de l'EPFL",
-    },
-    {
-      'title': "Introduction à Firebase",
-      'date': "24.04.2024",
-      'location': "Salle de conférence de l'HES-SO",
-    },
-  ];
+class NotificationsPage extends StatelessWidget {
+  const NotificationsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 30,
-        ),
-        const Text(
-          "Événements à venir",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: _events.length,
-            itemBuilder: (context, index) {
-              final title = _events[index]['title'];
-              final date = _events[index]['date'];
-              final location = _events[index]['location'];
-
-              return Card(
-                color: Colors.white,
-                elevation: 2,
-                child: ListTile(
-                  title: Text("$title"),
-                  subtitle: Text("$date - $location"),
-                ),
-              );
-            }
-          ),
-        ),
-      ],
-    );
+    return const Placeholder();
   }
 }
->>>>>>> bottom-navigation-bar
