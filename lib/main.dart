181d2be331c93b2f1bac3ec1444d7d73be3f4efb
Zilloww <raphaelbrancovieira.pro@gmail.com>
2024-02-20 14:45:43 +0100

    ui(details-page): Init UI of the events page

--- lib/main.dart
+++ lib/main.dart
@@ -54,11 +54,11 @@ class _AsynconfAppState extends State<AsynconfApp> {
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
-              label: "Accueil"
+              label: "Accueil",
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.event),
-              label: "Événements"
+              label: "Événements",
             ),
           ]
         ),
@@ -67,11 +67,80 @@ class _AsynconfAppState extends State<AsynconfApp> {
   }
 }
 
-class EventsPage extends StatelessWidget {
+class EventsPage extends StatefulWidget {
   const EventsPage({super.key});
 
   @override
+  State<EventsPage> createState() => _EventsPageState();
+}
+
+class _EventsPageState extends State<EventsPage> {
+
+  final _events = [
+    {
+      'title': "Conférence sur le développement durable",
+      'date': "20.04.2024",
+      'location': "Salle de conférence de l'EPFL",
+    },
+    {
+      'title': "Git Basics",
+      'date': "22.04.2024",
+      'location': "Salle de conférence de l'EPFL",
+    },
+    {
+      'title': "Introduction à Flutter",
+      'date': "23.04.2024",
+      'location': "Salle de conférence de l'EPFL",
+    },
+    {
+      'title': "Introduction à Dart",
+      'date': "23.04.2024",
+      'location': "Salle de conférence de l'EPFL",
+    },
+    {
+      'title': "Introduction à Firebase",
+      'date': "24.04.2024",
+      'location': "Salle de conférence de l'HES-SO",
+    },
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        const SizedBox(
+          height: 30,
+        ),
+        const Text(
+          "Événements à venir",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _events.length,
+            itemBuilder: (context, index) {
+              final title = _events[index]['title'];
+              final date = _events[index]['date'];
+              final location = _events[index]['location'];
+
+              return Card(
+                color: Colors.white,
+                elevation: 2,
+                child: ListTile(
+                  title: Text("$title"),
+                  subtitle: Text("$date - $location"),
+                ),
+              );
+            }
+          ),
+        ),
+      ],
+    );
   }
 }
